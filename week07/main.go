681ddc195aa6146811f2c4c24145be9759ac5296
koreng1019 <10min1019@gmail.com>
2024-10-11 18:28:43 +0900

    v1.1 Shadowing

--- week07/main.go
+++ week07/main.go
@@ -11,10 +11,17 @@ func main() {
 	fmt.Print("이름 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	name, err := in.ReadString('\n')
+	// Shadowing problem
+	// var float32 float32 = 9.1
+	// fmt.Println(float32)
+	// var number float32
+	// fmt.Println(number)
 
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		fmt.Println(name)
 	}
+	// var fmt float32 = 9.1
+	// fmt.Println(float32)
 }
